a3d1d4042a448ddb99991423412291f3a1de4187
Nick Paraschou <niparasc@gmail.com>
2018-07-11 07:53:20 -0400

    Adding company component and company entity

---
+++ src/app/domain/company.ts
@@ -0,0 +1,10 @@
+export class Company {
+    id: number;
+    name: string;
+    address: string;
+    city: string;
+    country: string;
+    email: string;
+    phone: string;
+    // array of BeneficialOwner here
+}
